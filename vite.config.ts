7bbea1e07483271ad379682a1fd5cab575ee605a
GopiRam <gopi.thungam.001@gmail.com>
2025-11-11 14:26:45 -0600

    base file name updated to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/MyPortfolio/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/MyPortfolio/' : '/',
+  base: "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
